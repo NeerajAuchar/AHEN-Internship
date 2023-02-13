62c620b869c3cf81ce08ffe3aaa55c8bf9ed5d37
Neeraj Auchar <neerajauchar@gmail.com>
2023-02-14 00:16:50 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Drive from "./components/Driving_classes/Driving"
+import Form from './components/Forms/Forms'
+
+
+export default function App() {
+  return (
+    <div>
+      < Drive /> 
+      {/* < Form /> */}
+    </div>
+
+  )
+}
+
---
+++ src/components/Driving_classes/Driving.js
@@ -0,0 +1,116 @@
+import React from 'react'
+import "./Driving.css"
+import Forms from '../Forms/Forms'
+
+function driving() {
+  return (
+    <React.Fragment>
+
+    <div className="container">
+      <div className="row">
+        <div className="card1" >
+
+          <img class="card-img-top" src="https://www.marutisuzukidrivingschool.com/blog/driving-school/marutisuzukidrivingschoolcdn.azureedge.net/-/media/feature/maruti-driving-school/blog/driving-tips/new/842-x-377.jpg?rev=a6723c4419ae4dd8af257e1fb602701f&modified=20200326090210&h=377&w=842&la=en&hash=48831EDE6B2A64E5ACB4F8147EE96F78" width="350px" alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title"><h1>Maruti Suzuki driving school </h1></h5>
+            <p className="card-text">Address : Bhosari bus terminal , Pune</p>
+            <button type='Button' onClick={Forms}>Book you class</button>
+          </div>
+        </div>
+
+
+
+
+        <div className="card2" >
+          <img class="card-img-top" src="https://th.bing.com/th/id/OIP.Vk9vouv-geEFig4FpFkCewHaEK?pid=ImgDet&w=600&h=337&rs=1" width="350px" alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title"><h1>Om Driving school</h1>  </h5>
+            <p className="card-text">Address : Dighi  , Pune</p>
+            <button type='Button' >Book you class</button>
+          </div>
+
+        </div>
+
+
+        <div className="card3" >
+          <img class="card-img-top" src="https://media.istockphoto.com/vectors/driving-lessons-in-driving-school-autoclass-vector-id1162801618?k=6&m=1162801618&s=170667a&w=0&h=dTqL6y-sJFKqSbI4hWYBSAx668Loo47W-gbpP-Zs9jw=" width="350px" alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title"><h1>Hrithik driving school</h1>  </h5>
+            <p className="card-text">Address : Near Phule Nagar RTO  , Pune</p>
+            <button type='Button' >Book you class</button>
+          </div>
+
+        </div> 
+
+
+        <div className="card4" >
+          <img class="card-img-top" src="https://th.bing.com/th/id/OIP.v--_VLs-T58aWn0DK2fFVAHaEK?pid=ImgDet&rs=1" width="250px" alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title"><h1>Gourav driving school </h1>  </h5>
+            <p className="card-text">Address : Ghorpadi   , Pune</p>
+            <button type='Button' >Book you class</button>
+          </div>
+
+        </div>
+
+
+        <div className="card5" >
+          <img class="card-img-top" src="https://netstorage-briefly.akamaized.net/images/7eb4cea0ee52ac8b.jpg" width="250px" alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title"><h1>Ahen driving school</h1>  </h5>
+            <p className="card-text">Address : Moshi  , Pune</p>
+            <button type='Button' >Book you class</button>
+          </div>
+
+        </div>
+
+        <div className="card6" >
+          <img class="card-img-top" src="https://autonexa.com/wp-content/uploads/2021/01/driving-school2.jpg" width="250px" alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title"><h1>Rahul driving school </h1>  </h5>
+            <p className="card-text">Address : Shanivar peth , Pune</p>
+            <button type='Button' >Book you class</button>
+          </div>
+
+        </div>
+
+        <div className="card7" >
+          <img class="card-img-top" src="https://lh5.googleusercontent.com/-hKnxVG4zTjE/WvrB3WEyPWI/AAAAAAAAAaI/9ZcJ0KeGxikYbpKZOQnDJvhn3bt8LsHMwCLIBGAYYCw/w1440-h810-k-no/" width="250px" alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title"><h1>Yogiraj driving school </h1>  </h5>
+            <p className="card-text">Address : Ravivar peth , Pune</p>
+            <button type='Button' >Book you class</button>
+          </div>
+
+        </div>
+
+        <div className="card8" >
+          <img class="card-img-top" src="https://im.whatshot.in/img/2020/Aug/bhagyawanti-motor-driving-school-hinjawadi-pune-motor-training-schools-3tkf0-1598412947.jpg" width="260px" alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title"><h1>Bhagyavati driving school </h1>  </h5>
+            <p className="card-text">Address : Chakan , Pune</p>
+            <button type='Button' >Book you class</button>
+          </div>
+
+        </div>
+
+
+        <div className="card9" >
+          <img class="card-img-top" src="https://sulcdn.azureedge.net/biz-live/img/2504688-525167-19092016172329.jpeg" width="260px" alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title"><h1>Pragati  driving school </h1>  </h5>
+            <p className="card-text">Address : Hadapsar  , Pune</p>
+            <button type='Button' >Book you class</button>
+          </div>
+
+        </div>
+
+
+
+      </div>
+    </div>
+  </React.Fragment>
+  )
+}
+
+export default driving
