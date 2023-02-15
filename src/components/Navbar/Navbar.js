17dc953f06107adaacdc0fcb7e99891dc980fc9b
Gaurav Pagare <gspagare03@gmail.com>
2023-02-16 01:10:38 +0530

    responsiveness added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import "./Navbar.css"
-import "../Driving_classes/Driving.css"
 import { FaBars } from 'react-icons/fa';
-import Driving from "../Driving_classes/Driving"
 
 
 
@@ -10,9 +8,8 @@ export default function Navbar() {
   return (
     <div className='navbody' style={{cursor: "pointer", margin: -10, }}>
 <div class="menu">
- <div style={{fontSize: 20, float: "right", padding:5}}>
+ <div className='icon' style={{fontSize: 20, float: "right", padding:5}}>
  <FaBars />
-
  </div>
 		<nav>
 			<ul>
